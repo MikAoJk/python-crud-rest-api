06b91fe5f2f723cb0e89ebad9122abb284facfc6
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2023-11-29 12:39:50 +0100

    added in flask

--- pythoncrudrestapi/__init__.py
+++ pythoncrudrestapi/__init__.py
@@ -1,6 +1,45 @@
-def main():
-    print('Hello Python World')
+import flask
+from flask import Flask, request, redirect
+
+app = Flask(__name__)
+
+
+items = []  # This list will store the items
+
+
+@app.route('/users/', methods=['GET'])
+def root():
+    return flask.Response(status=200, response='Hello world')
+
+@app.route('/users', methods=['POST'])
+def create():
+    id = request.form['id']
+    name = request.form['name']
+    email = request.form['name']
+    items.append(name)
+    return flask.Response(status=200)
+
+@app.route('/users', methods=['GET'])
+def root():
+    return flask.Response(status=200, response='Hello world')
+
+@app.route('/users/', methods=['PUT'])
+def update():
+    old_name = request.form['old_name']
+    new_name = request.form['new_name']
+    if old_name in items:
+        index = items.index(old_name)
+        items[index] = new_name
+    return flask.Response(status=200)
+
+@app.route('/delete', methods=['DELETE'])
+def delete():
+    name = request.form['name']
+    if name in items:
+        items.remove(name)
+    return flask.Response(status=200)
 
 
 if __name__ == "__main__":
-    main()
+    app.run(host='0.0.0.0', port=8080)
+
