03b7ec27772f2241cd186c4c795573804f6a17cb
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2023-11-29 15:14:41 +0100

    Small clean up

--- pythoncrudrestapi/__init__.py
+++ pythoncrudrestapi/__init__.py
@@ -3,12 +3,8 @@ from flask import Flask, request, redirect
 
 app = Flask(__name__)
 
-
-items = []  # This list will store the items
-
-
 @app.route('/users/', methods=['GET'])
-def root():
+def users():
     return flask.Response(status=200, response='Hello world')
 
 @app.route('/users', methods=['POST'])
@@ -16,30 +12,23 @@ def create():
     id = request.form['id']
     name = request.form['name']
     email = request.form['name']
-    items.append(name)
     return flask.Response(status=200)
 
 @app.route('/users', methods=['GET'])
-def root():
+def user():
     return flask.Response(status=200, response='Hello world')
 
 @app.route('/users/', methods=['PUT'])
 def update():
     old_name = request.form['old_name']
     new_name = request.form['new_name']
-    if old_name in items:
-        index = items.index(old_name)
-        items[index] = new_name
     return flask.Response(status=200)
 
 @app.route('/delete', methods=['DELETE'])
 def delete():
     name = request.form['name']
-    if name in items:
-        items.remove(name)
     return flask.Response(status=200)
 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
-
